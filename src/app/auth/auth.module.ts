02bf2ce27e5aa18a3276eb97b2c8951e9ca9399f
Jorge Cano <jorgeucano@gmail.com>
2018-04-02 14:38:49 -0300

    order imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,23 +1,21 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// modules
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-
-
+import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+// services
 import { AuthService } from './services/auth.service';
 import { AuthGuardService } from './services/auth-guard.service';
+// ngrx
 import { AuthEffects } from './effects/auth.effects';
-
-import { LoginComponent } from './login/login.component';
-
-import { AuthRoutingModule } from './auth-routing.module';
-
 import { reducers } from './reducers';
-import { MaterialModule } from '../material/material.module';
-
+// components
+import { LoginComponent } from './login/login.component';
 
 export const COMPONENTS = [LoginComponent];
 
